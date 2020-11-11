5d2b943f82b667dc9211b1c411a101a7c080566c
Eredion <alvaro@DESKTOP-U3PRNDM.localdomain>
2020-11-11 20:01:33 +0100

    Square root added

--- main.js
+++ main.js
@@ -1,21 +1,63 @@
-const abs = (n) => { return (n >= 0 ? n : n * -1) }
+const abs = (n) => { return (n >= 0 ? n : n * -1) } // Absolute value function.
+
+const sqrt = (t) =>
+{ // Square root using Newthon's Method with a max of 1000 iterations.
+	let r = t / 2.0;
+	for (let i = 0; i <= 1000 ; ++i)
+		r = (r + t / r ) / 2;
+	return r;
+}
+
 class Equation
 {
-	arrx = [0]; // This variable saves all degrees of x  
+	constructor() {this.arrx = [];} // This variable saves all degrees of x
+
 	addMonomial(coef, degree)
 	{
 		while (this.arrx.length <= degree)
 			this.arrx.push(0);
 		this.arrx[degree] += coef;
 	}
-	print ()
+
+	print()
 	{
-		let msg = `Reduced form: ${this.arrx[0] < 0? '- ':''}${abs(this.arrx[0])} * X^${0}`;
+		let msg = `Reduced form: ${this.arrx[0] < 0? '- ':''}${abs(this.arrx[0])} * X^${0} `;
 		for (let i = 1; i < this.arrx.length; ++i)
 			msg += `${this.arrx[i] < 0? '- ':'+ '}${abs(this.arrx[i])} * X^${i}`
 		console.log(msg + " = 0");
 	}
+
 	get degree(){ return (this.arrx.length - 1)}
+	get discriminant()
+	{
+		if (this.degree != 2)
+			return false;
+		return (-4 * this.arrx[2] * this.arrx[0]);
+	}
+
+	get discriminantInfo()
+	{
+
+		if (this.discriminant === 0)
+			console.log("Discriminant is zero, the solution is:");
+		else if (this.discriminant > 0)
+			console.log("Discriminant is strictly positive, the two solutions are:");
+		else
+			console.log("Discriminant is strictly negative, this equation has not solution");
+	}
+
+	get solution()
+	{
+		if (this.degree === 1)
+			return (this.arrx[0] / -this.arrx[1]);
+
+	}
+
+	printSolution()
+	{
+		if (this.degree === 1)
+			console.log("The solution is:\n" + this.solution);
+	}
 };
 
 const readInput = (str) => {
@@ -23,7 +65,7 @@ const readInput = (str) => {
 	let hallado;
 	let equ = new Equation();
 	let eq = 0;
-	while ((hallado = x_reg.exec(str)) !== null) { 
+	while ((hallado = x_reg.exec(str)) !== null) {
 		let degree = parseInt(hallado[5]);
 		let coef = parseFloat(hallado[3]);
 		if (hallado[2] === '- ')
@@ -45,4 +87,10 @@ let equ = readInput(process.argv[2]);
 equ.print();
 console.log("Polynomial degree: " + equ.degree);
 if (equ.degree > 2)
+{
 	console.log("The polynomial degree is strictly greater than 2, I can't solve.");
+	return;
+}
+equ.discriminantInfo;
+equ.printSolution();
+
