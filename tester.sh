d9573c552e2c25e9318e9fa12c446dbbc9b9897e
Eredion <alvaro.segovia@hotmail.com>
2021-03-10 09:27:23 +0100

    Name changes

---
+++ tester.sh
@@ -0,0 +1,12 @@
+node computor.js "5 * X^0 = 5 * X^0";
+echo "\n"
+node computor.js "4 * X^0 = 8 * X^0";
+echo "\n"
+node computor.js "5 * X^0 = 4 * X^0 + 7 * X^1";
+echo "\n"
+node computor.js "5 * X^0 + 13 * X^1 + 3 * X^2 = 1 * X^0 + 1 * X^1";
+echo "\n"
+node computor.js "6 * X^0 + 11 * X^1 + 5 * X^2 = 1 * X^0 + 1 * X^1";
+echo "\n"
+node computor.js "5 * X^0 + 3 * X^1 + 5 * X^2 = 1 * X^0 + 1 * X^1";
+echo "\n"
